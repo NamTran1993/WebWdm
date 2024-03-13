dfe017b47225091204a6882a897ce5e43b494170
Tuyen Nguyen <dinhtona@gmail.com>
2024-03-13 15:00:01 +0700

    Finished DB Settings page

---
+++ WebSiteWDM/App_Code/Ajax/SettingsData.cs
@@ -0,0 +1,25 @@
+﻿using System.Web;
+
+public class SettingsData : IHttpHandler
+{
+    public bool IsReusable { get { return false; } }
+    
+
+    public void ProcessRequest(HttpContext context)
+    {
+        string jsonRes = string.Empty;
+        string func = context.Request.QueryString["func"];
+        switch (func)
+        {
+            case "DB-SETTINGS":
+                {
+                    jsonRes = SettingsBLL.GetDbSettings();
+                    break;
+                }
+            default: break;
+        }
+
+        context.Response.ContentType = "text/plain";
+        context.Response.Write(jsonRes);
+    }
+}
---
+++ WebSiteWDM/Scripts/settingsPage.js
@@ -0,0 +1,69 @@
+﻿var app = angular.module('app', []);
+var scope = null;
+var url = '../Ajax/SettingsData.ashx';
+
+app.controller('settingsController', function ($scope) {
+    console.log('settingsController');
+
+    this.scope = $scope;
+
+    $scope.applyDelay = null;
+    $scope.updateUI = function () {
+        try {
+            if ($scope.applyDelay !== null) {
+                clearTimeout($scope.applyDelay);
+            }
+
+            $scope.applyDelay = setTimeout(function () {
+                $scope.$apply();
+                loadingUI.Close();
+            }, 500);
+        }
+        catch (e) {
+            console.log('$scope.applyDelay Exception:');
+        }
+    };
+
+    // ----------#-----------
+    $scope.pageIndex = 1;
+    $scope.recordPerPage = 1000;
+    $scope.dbSettings = [];
+
+    $scope.search = function () {
+        console.log('search');
+
+        let jsonReq = {};
+
+        $.ajax({
+            type: "POST",
+            url: url + "?func=DB-SETTINGS",
+            data: jsonReq,
+
+            beforeSend: function () {
+                loadingUI.Open();
+            },
+
+            success: function (jsonRes) {
+                try {
+                    $scope.dbSettings = JSON.parse(jsonRes);
+                    $scope.updateUI();
+
+                } catch (e) {
+                    loadingUI.Close();
+                    console.error('=>> ERROR: ' + e);
+                }
+            },
+
+            error: function (xhr, textStatus, error) {
+                loadingUI.Close();
+            }
+        });
+    };
+
+    $scope.searchDetail = function (levelLog, info) {
+        console.info('LEVEL LOG: ' + levelLog + ' INFO: ' + JSON.stringify(info));
+
+    };
+
+    setTimeout($scope.search, 500);
+});
